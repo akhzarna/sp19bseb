5fd5f17bef445bfd44caf400cbdc31a84ccc6ab6
tahirhameed99 <tahir.hameed99@gmail.com>
2022-02-21 09:48:02 +0500

    New Commit

--- App.js
+++ App.js
@@ -16,50 +16,39 @@ export default class App extends React.Component {
   render(){
     return (
 
-    <View style={{backgroundColor:'grey', flex:1}}>
-
-      <View style={{backgroundColor:'red', flex:0.25}}>
-
-      <Text style={{color:'white'}}>
-                   Usama Working SP19 BSE C First Commit Login
-      </Text>
-
-      </View>
-
-      <View style={{backgroundColor:'green', flex:0.35}}>
-
-
-      <TextInput style={{backgroundColor:'white'}}
-      />
-
-      </View>
-
-      <View style={{backgroundColor:'blue', flex:0.45}}>
+      <View style={{flex:1, flexDirection:'row'}}>
 
 
 
 
+      
 
+        <View style={{flex:0.20}}>
+          <Text> Login Screen </Text>
 
+        </View>
+        <View style={{flex:0.40}}>
+          <TextInput style={{backgroundColor:'lightgrey', marginBottom:10}} />
+          <TextInput style={{backgroundColor:'lightgrey'}} />
 
+        </View>
+        <View style={{flex:0.40}}>
+          <Button
+          title="Sin In"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+          />
 
+          <Button
+          title="Sin Up"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+          />
 
-
-      <Button
-      title="Sin In"
-      color="#841584"
-      accessibilityLabel="Learn more about this purple button"
-      />
-
-      <Button
-      title="Sign Up"
-      color="#841584"
-      accessibilityLabel="Learn more about this purple button"
-      />
+        </View>
 
       </View>
 
-    </View>
     );
   }
 }
@@ -67,3 +56,44 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
 
 });
+
+
+
+
+
+
+// <View style={{backgroundColor:'grey', flex:1}}>
+//
+//   <View style={{backgroundColor:'red', flex:0.25}}>
+//
+//   <Text style={{color:'white'}}>
+//                Usama Working SP19 BSE C First Commit Login
+//   </Text>
+//
+//   </View>
+//
+//   <View style={{backgroundColor:'green', flex:0.35}}>
+//
+//
+//   <TextInput style={{backgroundColor:'white'}}
+//   />
+//
+//   </View>
+//
+//   <View style={{backgroundColor:'blue', flex:0.45}}>
+//
+//   <Button
+//   title="Sin In"
+//   color="#841584"
+//   accessibilityLabel="Learn more about this purple button"
+//   />
+//
+//   <Button
+//   title="Sign Up"
+//   color="#841584"
+//   accessibilityLabel="Learn more about this purple button"
+//   />
+//
+//   </View>
+//
+// </View>
